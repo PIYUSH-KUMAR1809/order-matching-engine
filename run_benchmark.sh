c549c371995ace19146556fc102b813be24c9157
piyush <kpiyush8826@gmail.com>
2025-12-21 12:50:14 +0530

    feat: created a benchmark script

---
+++ run_benchmark.sh
@@ -0,0 +1,15 @@
+# Delete current build diectory
+rm -rf ./build
+
+# Create new build directory
+mkdir -p build
+cd build
+
+# Generate MakeFile
+cmake ..
+
+# Create executable
+make
+
+# Run benchmark
+./src/benchmark
